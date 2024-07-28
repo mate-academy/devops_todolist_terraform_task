8cf61fd7d2c62e929300eac4773b54d741cfff74
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-28 12:47:09 +0300

    Fixed fmt main.tf, deleted unused files

--- modules/compute/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "network_interface_id" {
-#   description = "The ID of the Network Interface"
-#   value       = azurerm_network_interface.network_interface.id
-# }
-
-# output "virtual_machine_id" {
-#   description = "The ID of the Virtual Machine"
-#   value       = azurerm_linux_virtual_machine.vm.id
-# }
