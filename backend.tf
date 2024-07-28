6af8a9d021250579767bd27a1e7c2bafaf8a5ff7
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-28 12:48:34 +0300

    Added backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "mate-azure-task-12"
-#     storage_account_name = "storageippk"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "mate-azure-task-12"
+    storage_account_name = "storageippk"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
