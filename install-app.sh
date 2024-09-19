747cfd348af4ae163536d4d6e7e8cf405e7e0c00
Serveladus <serveladik@gmail.com>
2024-09-19 11:44:44 +0300

    Solution0.96

--- install-app.sh
+++ install-app.sh
@@ -12,6 +12,10 @@ sudo apt-get install python3-pip -yq
 
 # Create a directory for the app and download the files. 
 sudo mkdir /home/azureuser/app
+
+sudo chown -R azureuser:azureuser /home/azureuser/devops_todolist_terraform_task
+sudo chown -R azureuser:azureuser /home/azureuser/app
+
 # make sure to uncomment the line bellow and update the link with your GitHub username
 # git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
 sudo cp -r /home/azureuser/devops_todolist_terraform_task/app/* /home/azureuser/app
