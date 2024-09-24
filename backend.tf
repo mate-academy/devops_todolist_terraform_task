f905c866e19603258b62b670932e0941c7e30d8e
Volodymyr Shutov <forshutov@gmail.com>
2024-09-25 00:39:04 +0400

    terraform init, plan, apply; uncomment backend; terraform init upgarde

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     storage_account_name = "shutftask12sa"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
-#     resource_group_name  = "mate-azure-task-12"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    storage_account_name = "shutftask12sa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "mate-azure-task-12"
+  }
+}
